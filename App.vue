1609bd119e841cad9302372d68252fc1fe645329
pouria <85546967+rahimi-pouria@users.noreply.github.com>
2023-01-04 16:18:21 +0330

    Add files via upload
    asd

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex flex-column w-100">
+    <HeaderComponent />
+  </div>
+</template>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
+<script>
+import HeaderComponent from "@/components/Header";
+export default {
+  components: {HeaderComponent}
+}
+</script>
