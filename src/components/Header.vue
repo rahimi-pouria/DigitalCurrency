33886dfd8b9405a6d13b8b082d9700df2368fe52
pouria <85546967+rahimi-pouria@users.noreply.github.com>
2023-01-04 16:17:21 +0330

    Add files via upload
    components

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex flex-column w-100 p-4 text-white" style="background: #1fe0ff">
+    <b-navbar type="dark" variant="dark" class="d-flex g16" style="gap: 18px;">
+      <router-link class="text-black text-decoration-none" to="/">Home</router-link>
+      <router-link class="text-black text-decoration-none" to="/SearchTransaction">SearchTransaction</router-link>
+    </b-navbar>
+  </div>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
